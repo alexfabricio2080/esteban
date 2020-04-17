66bac4ee39a1e19da094cba17738865db451673a
alexfabricio2080 <42243036+alexfabricio2080@users.noreply.github.com>
2020-04-17 15:55:04 -0600

    Add files via upload

--- enviar2.php
+++ enviar2.php
@@ -1,7 +1,5 @@
 <?php
 $nombre = $_POST['nombre'];
-$opcion = $_POST['inlineRadioOptions'];
-$Fecha = $_POST['fecha'];
 $mail = $_POST['correo'];
 $telefono = $_POST['tel'];
 $empresa = $_POST['mensaje'];
@@ -12,8 +10,6 @@ $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
 
 $mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
-$mensaje = "1: Partido, 2: Entrenamiento: " . $opcion . ",\r\n";
-$mensaje = "Fecha del partido o el entrenamiento: " . $Fecha . ",\r\n";
 $mensaje .= "Su e-mail es: " . $mail . " \r\n";
 $mensaje .= "Su teléfono es: " . $telefono . " \r\n";
 $mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
