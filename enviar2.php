c2cf4b491d2bc2455dc94ea7d4e75e0b01283229
alexfabricio2080 <42243036+alexfabricio2080@users.noreply.github.com>
2020-04-17 23:06:02 -0600

    Add files via upload

--- enviar2.php
+++ enviar2.php
@@ -11,8 +11,6 @@ $header = "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header = "Mime-Version: 1.0 \r\n";
 $header = "Content-Type: text/plain";
 
-
-
 $mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
 $mensaje = "1: Partido, 2: Entrenamiento: " . $opcion . ",\r\n";
 $mensaje = "Fecha del partido o el entrenamiento: " . $Fecha . ",\r\n";
@@ -21,9 +19,8 @@ $mensaje = "Su teléfono es: " . $telefono . " \r\n";
 $mensaje = "Mensaje: " . $_POST['mensaje'] . " \r\n";
 $mensaje = "Enviado el " . date('d/m/Y', time());
 
-
 $para = 'alexfabricio2080@gmail.com';
-$asunto = 'Solicitud de información';
+$asunto = 'Esfuerzos';
 
 mail($para, $asunto, utf8_decode($mensaje), $header);
 
